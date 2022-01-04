11812490987f66dfe03924c6abbb18b314d70342
eisa <g45@outlook.com>
2022-01-04 11:40:20 +0200

    Targeting System
    auto target closest enemy +
    shoot on aim only

---
+++ Assets/Bank/Bank.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bank : MonoBehaviour
+{
+    [SerializeField] int startingBalance = 150;
+    [SerializeField] int currentBalance;
+    void Awake()
+    {
+        currentBalance = startingBalance;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Deposit(int amount)
+    {
+        currentBalance += Mathf.Abs(amount);
+    }
+    public void Withdraw(int amount)
+    {
+        currentBalance -= Mathf.Abs(amount);
+    }
+}
--- Assets/Towers/Targeting.cs
+++ Assets/Towers/Targeting.cs
@@ -6,17 +6,60 @@ public class Targeting : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    Transform turret;
+    [SerializeField] Transform turret;
+    [SerializeField] ParticleSystem bolts;
+    
     Transform target;
-    void Start()
-    {
-        turret = transform.Find("BallistaTopMesh").transform;
-        target = FindObjectOfType<PathFinder>().transform;
-    }
+
+    [SerializeField] float range = 15f;
+
 
     // Update is called once per frame
     void Update()
     {
-        turret.LookAt(target);
+        FindNearestEnemy();
+
+        aimWeapon();
     }
+
+    void FindNearestEnemy()
+    {
+
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+
+        Transform currentNearestTarget = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach (Enemy enemy in enemies)
+        {
+            float currentDistance = Vector3.Distance(this.transform.position, enemy.transform.position);
+
+            if( currentDistance <= maxDistance)
+            {
+                currentNearestTarget = enemy.transform;
+                maxDistance = currentDistance;
+            }
+        }
+        target = currentNearestTarget;
+    }
+    void aimWeapon()
+    {
+        if(target != null)
+        {
+            float distance = Vector3.Distance(this.transform.position, target.position);
+            
+
+            var emission = bolts.emission;
+
+            if(distance <= range)
+            {
+                turret.LookAt(target);
+                emission.enabled = true;
+            }
+            else{
+                emission.enabled = false;
+            }
+        }
+    }
+
 }
